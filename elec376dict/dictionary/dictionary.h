b06caf969e7de2f1fc60cc40752c589ab0785200
Connor Gorman (21CRG6) <21crg6@queensu.ca>
2024-11-22 01:29:49 +0000

    Update dictionary.h

--- elec376dict/dictionary/dictionary.h
+++ elec376dict/dictionary/dictionary.h
@@ -2,6 +2,7 @@
 #define DICTIONARY_H
 
 #include <QMainWindow>
+#include "dict.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -14,10 +15,19 @@ class Dictionary : public QMainWindow
     Q_OBJECT
 
 public:
-    Dictionary(QWidget *parent = nullptr);
+    explicit Dictionary(QWidget *parent = nullptr);
     ~Dictionary();
+
+private slots:
+    void on_registerStudentButton_clicked();
+    void on_markCompletedButton_clicked();
+    void on_markUncompletedButton_clicked();
+    void on_showModulesButton_clicked();
+    void on_getDefinitionButton_clicked();
 
 private:
     Ui::Dictionary *ui;
+    Dict dictionary; // Instance of the backend dictionary class
 };
+
 #endif // DICTIONARY_H
